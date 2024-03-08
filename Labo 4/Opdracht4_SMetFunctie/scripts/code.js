e725f95a184d0742f007f03b43aa27805574d443
ZoeTermont <zoe.termont@student.vives.be>
2024-03-09 00:19:29 +0100

    Labo4Fix

--- Labo 4/Opdracht4_SMetFunctie/scripts/code.js
+++ Labo 4/Opdracht4_SMetFunctie/scripts/code.js
@@ -1,21 +1,21 @@
 const setup = () => {
-    const spatiesButton = document.getElementById("spaties");
-    spatiesButton.addEventListener("click", addSpaties);
+    const spatiesn = document.getElementById("spaties");
+    spaties.addEventListener("click", addSpaties);
 }
 
 const addSpaties = () => {
-    const input = document.getElementById("input");
-    const output = document.getElementById("output");
+    const inputString = document.getElementById("input");
+    const outputString = document.getElementById("output");
 
-    if (input && output) {
-        const input = input.value;
+    if (inputString && outputString) {
+        const input = inputString.value;
         let nieuweString = "";
 
         for (let i = 0; i < input.length; i++) {
             nieuweString += input.charAt(i) + " ";
         }
 
-        output.textContent = nieuweString;
+        outputString.textContent = nieuweString;
         console.log(nieuweString);
     }
 }
